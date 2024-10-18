2307b661debc8260eaaa81d4e6ece84b9cd4f404
Hugo Vincent <hugo.vincent@arm.com>
2024-10-18 18:27:12 +0100

    Improve toolchain portability of symbol name.

--- impl.h
+++ impl.h
@@ -6,5 +6,4 @@ void f1600(uint64_t a[25]);
 
 extern uint64_t RC[24];
 
-extern void f1600x2(uint64_t* a, uint64_t* rc);
-extern void f1600x2b(uint64_t* a, uint64_t* rc);
+extern void f1600x2(uint64_t* a, uint64_t* rc) asm("_f1600x2");
